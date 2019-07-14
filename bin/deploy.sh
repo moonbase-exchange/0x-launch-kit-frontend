bd28b5c49bc91a8b7667ed9e3dc1e6cfce80dd55
Aleksandr Kuzmenko <aleksandr.v.kuzmenko@gmail.com>
2019-07-14 08:42:36 +0300

    Add deployment script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,1 +1,19 @@
 #!/usr/bin/env bash
+
+HOST=do.outerface
+REMOTE_DIR=0x-launch-kit/
+IMAGE_TAG=0x-launch-kit-frontend
+TMP_IMAGE_NAME=0x-launch-kit-frontend.img
+TMP_IMAGE_FILE=/tmp/${TMP_IMAGE_NAME}
+
+docker build -t ${IMAGE_TAG} .
+docker save ${IMAGE_TAG} -o ${TMP_IMAGE_FILE}
+scp ${TMP_IMAGE_FILE} ${HOST}:${REMOTE_DIR}
+
+ssh ${HOST} << EOF
+    cd ${REMOTE_DIR}
+    docker load -i ${TMP_IMAGE_NAME}
+    docker-compose up -d
+EOF
+
+rm ${TMP_IMAGE_FILE}
